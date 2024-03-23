300ca79cd2e6d330267770b0ed113c80773b32cf
Peter Nagy <155898084+Skoteinos1@users.noreply.github.com>
2024-03-23 21:19:08 +0000

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,27 @@
-import ollama
+import time
+from threading import Event, Thread  
 import os
-from Threading import Threads
+import playsound
+import ollama
+
+
+chat_memory = '\n\nCHAT HISTORY:\n'
+
+# talking_library = 'pyttsx3'
+talking_library = 'TTS.api'
+# talking_library = 'gTTS'
+# talking_library = 'os'
+
+# https://ollama.com/library?sort=popular
+llm_model = 'zephyr'  # too much moralizing
+llm_model = 'llama2-uncensored'  # for talking
+# llm_model = 'codellama'  # for coding
+# llm_model = 'starcoder'  # maybe for coding co-pilot
+# llm_model = 'samantha-mistral'  # Shrink
+
+# curl -fsSL https://ollama.com/install.sh | sh
+# ollama run llama2-uncensored
+
+set_laguage = 'en'
+# set_langage = 'hu'
+# set_laguage = 'sk' 
